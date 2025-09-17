86231552669df920335655a9131efd3602996cf3
Abhishek Singh <aksinghpro77@gmail.com>
2025-09-17 22:32:46 +0530

    removed vowel from string

---
+++ codingDrill1/removeVowelsFromString.js
@@ -0,0 +1,16 @@
+const string = "This is test string to remove vowels";
+
+const removeVowels = (str) => {
+  const vowels = ["a", "e", "i", "o", "u"];
+  if (!str) return false;
+  let updatedStr = "";
+  for (let char of str) {
+    if (!vowels.includes(char)) {
+      updatedStr += char;
+    }
+  }
+  return updatedStr;
+};
+
+const removedVowel = removeVowels(string);
+console.log(removedVowel);
