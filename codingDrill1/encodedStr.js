884338adb2fb27d8ce83535e805db4f523c136dd
Abhishek <abhishek@Abhisheks-MacBook-Air.local>
2025-11-25 22:00:30 +0530

    encoded string

--- codingDrill1/encodedStr.js
+++ codingDrill1/encodedStr.js
@@ -1,14 +1,14 @@
-const str = "Abhishek";
+const str = "Abhishez";
 
-const encodeString = (str) => {
+const encodeString = (str, shiftBy) => {
   if (!str) return;
   let encodedStr = "";
   for (i = 0; i < str.length; i++) {
-    encodedStr += String.fromCharCode(str.charCodeAt(i) + 2);
+    encodedStr += String.fromCharCode(str.charCodeAt(i) + shiftBy);
   }
 
   return encodedStr;
 };
 
-const encodedStr = encodeString(str);
+const encodedStr = encodeString(str, 2);
 console.log(encodedStr);
